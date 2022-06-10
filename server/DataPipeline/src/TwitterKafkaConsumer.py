8ed5eb2d6387e2d2049c906d2c7df38964dd98c9
Aqudi <gjdigj145@gmail.com>
2022-06-10 16:20:01 +0900

    Implement data pipeline

--- server/DataPipeline/src/TwitterKafkaConsumer.py
+++ server/DataPipeline/src/TwitterKafkaConsumer.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 import sys
-import time
 import json
 from datetime import datetime
 
-from pyspark.streaming import StreamingContext
-
-from pyspark.sql import SparkSession, Row
-from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, decode, from_json, to_timestamp
 from pyspark.sql.types import *
 
-# import pyspark_cassandra # saveToCassandra 함수, package 추가 필요
 
 # 처리를 위해 필요한 스키마 1. Tweet 2. User 3. Quoted
 tweet_schema = [
@@ -24,45 +20,26 @@ tweet_schema = [
     "timestamp",
 
     # 따로 처리해야하는 내용들
-    'hashtags',  # entities.hashtags.tag
+    'hashtags',  # hashtags.tag
     "retweet_count",  # public_metrics.retweet_count
     "reply_count",  # public_metrics.reply_count
     "like_count",  # public_metrics.like_count
     "quote_count",  # public_metrics.quote_count
 ]  # user, quoted_status, in_reply_to_status_id 은 따로 처리
 
-
-# JSON 형태의 RDD 필터링 함수
-def tweet_filter(RDD):
-    filtered = {}
-    try:
-        # tweet_schema 처리
-        def tweet_lambda(x): return (x in tweet_schema)
-        existing_keys = list(filter(tweet_lambda, RDD.keys()))
-        for key in existing_keys:
-            filtered[key] = RDD[key]
-
-        # time 정보 추가
-        filtered["timestamp"] = int(time.time()*1000000)
-
-        # 추가 정보
-        filtered['hashtags'] = [hashtag["tag"]
-                                for hashtag in RDD["entities"]["hashtags"]]  # entities.hashtags.tag
-        # public_metrics.retweet_count
-        filtered["retweet_count"] = RDD["public_metrics"]["retweet_count"]
-        # public_metrics.reply_count
-        filtered["reply_count"] = RDD["public_metrics"]["reply_count"]
-        # public_metrics.like_count
-        filtered["like_count"] = RDD["public_metrics"]["like_count"]
-        # public_metrics.quote_count
-        filtered["quote_count"] = RDD["public_metrics"]["quote_count"]
-
-    except:
-        print("**********TWEET FILTER ERROR OCCUR**********")
-        print("Unexpected error:", sys.exc_info())
-        print(RDD)
-
-    return filtered
+schema = StructType([
+    StructField("id", LongType(), False),
+    StructField("text", StringType(), False),
+    StructField("author_id", LongType(), False),
+    StructField("created_at", StringType(), False),
+    StructField("lang", StringType(), True),
+    StructField("timestamp", TimestampType(), False),
+    StructField("hashtags", StringType(), True),
+    StructField("retweet_count", IntegerType(), True),
+    StructField("reply_count", IntegerType(), True),
+    StructField("like_count", IntegerType(), True),
+    StructField("quote_count", IntegerType(), True),
+])
 
 
 if __name__ == "__main__":
@@ -81,7 +58,7 @@ if __name__ == "__main__":
     # Initialize namespace and table
     spark.sql("CREATE DATABASE IF NOT EXISTS mycatalog.test WITH DBPROPERTIES (class='SimpleStrategy',replication_factor='1')")
     spark.sql("drop table mycatalog.test.testtable")
-    spark.sql("""CREATE TABLE mycatalog.test.testtable (   
+    spark.sql("""CREATE TABLE mycatalog.test.testtable (
                     id bigint,
                     text string,
                     author_id bigint,
@@ -105,10 +82,15 @@ if __name__ == "__main__":
         .option("kafka.bootstrap.servers", "kafka:9092") \
         .option("subscribe", "tweets") \
         .load()
-
-    ds = df.writeStream \
-        .foreach(tweet_filter) \
+    ds = df \
+        .withColumn("value", decode("value", "utf8")) \
+        .withColumn("value", from_json("value", schema)) \
+        .select("value.*") \
+        .withColumn("created_at", to_timestamp("created_at", "yyyy-MM-dd HH:mm:ss Z")) \
+        .withColumn("timestamp", current_timestamp()) \
+        .writeStream \
         .format("org.apache.spark.sql.cassandra") \
         .outputMode("append") \
         .options(table="testtable", keyspace="test", checkpointLocation="./cassandra_checkpoints") \
-        .start()
+        .start() \
+        .awaitTermination()
--- server/DataPipeline/src/TwitterKafkaProducer.py
+++ server/DataPipeline/src/TwitterKafkaProducer.py
@@ -15,30 +15,34 @@ load_dotenv(verbose=True)
 
 logger = logging.getLogger("kafka_producer")
 
+
 class Producer:
     def __init__(self, bootstrap_servers):
         logger.info("Kafka Producer Object Create")
         try:
             self.producer = KafkaProducer(
-                bootstrap_servers = bootstrap_servers,
-                max_block_ms = 10000, 
-                retries = 0, # default value
-                acks = 1, # default value
-                value_serializer = lambda v: json.dumps(v, ensure_ascii=False).encode("utf-8")
+                bootstrap_servers=bootstrap_servers,
+                max_block_ms=10000,
+                retries=0,  # default value
+                acks=1,  # default value
+                value_serializer=lambda v: json.dumps(
+                    v, ensure_ascii=False).encode("utf-8")
             )
         except KafkaError as exc:
-            logger.error("kafka producer - Exception during connecting to broker - {}".format(exc))
+            logger.error(
+                "kafka producer - Exception during connecting to broker - {}".format(exc))
             return False
-        
+
     def stop(self):
         self.producer.close()
 
     def send_data(self, topic, data):
         # Asynchronous by default
-        future = self.producer.send(topic, data).add_callback(self.on_send_success).add_errback(self.on_send_error)
+        future = self.producer.send(topic, data).add_callback(
+            self.on_send_success).add_errback(self.on_send_error)
         # block until all async messages are sent
         self.producer.flush()
-    
+
     def on_send_success(self, record_metadata):
         logger.info("**********Send Success***********")
         logger.info(f"record_metadata.topic: {record_metadata.topic}")
@@ -49,7 +53,7 @@ class Producer:
         logger.info("**********Send Error Occur**********")
         logger.error("I am an errback", exc_info=excp)
 
-        
+
 class TwitterStreamingClient(tweepy.StreamingClient):
     """ A class to read the twitter stream and push it to Kafka"""
 
@@ -57,10 +61,31 @@ class TwitterStreamingClient(tweepy.StreamingClient):
         super(TwitterStreamingClient, self).__init__(**kwargs)
         self.producer = Producer(bootstrap_servers="kafka:9092")
 
+    def parseInt(self, string):
+        try:
+            return int(string)
+        except:
+            return 0
+
     def on_tweet(self, tweet: tweepy.Tweet):
         try:
-            print(json.dumps(tweet.data).encode("utf-8"))
-            self.producer.send_data("tweets", tweet.data)
+            hastags = ""
+            if "hashtags" in (tweet.entities or {}):
+                hastags = ",".join(
+                    [tagDict["tag"] for tagDict in tweet.entities.get("hashtags")])
+            data = {
+                "id": tweet.id,
+                "text": tweet.text,
+                "author_id": tweet.author_id,
+                "created_at": tweet.created_at.strftime("%Y-%M-%d %H:%m:%S %z"),
+                "lang": tweet.lang,
+                "hashtags": hastags,
+                "retweet_count": tweet.public_metrics["retweet_count"],
+                "reply_count": tweet.public_metrics["reply_count"],
+                "like_count": tweet.public_metrics["like_count"],
+                "quote_count": tweet.public_metrics["quote_count"],
+            }
+            self.producer.send_data("tweets", data)
         except KafkaError as e:
             logger.error(e)
             return False
@@ -72,10 +97,11 @@ class TwitterStreamingClient(tweepy.StreamingClient):
     def on_timeout(self):
         return True  # Don't kill the stream
 
-    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--rules", nargs="+", help="필터링할 rule, hashtag (띄어쓰기로 구분)", default=[])
+    parser.add_argument("--rules", nargs="+",
+                        help="필터링할 rule, hashtag (띄어쓰기로 구분)", default=[])
     args = parser.parse_args()
     logger.info(args)
 
@@ -83,7 +109,7 @@ if __name__ == "__main__":
         format="%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s",
         level=logging.INFO
     )
-    
+
     BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
     # Create stream and bind the listener to it
@@ -98,7 +124,7 @@ if __name__ == "__main__":
 
     for track in args.rules:
         stream.add_rules(tweepy.StreamRule(track))
-    
+
     res = stream.get_rules()
     if res.data:
         msg = "\n\n========== Streaming rules ==========\n"
@@ -106,7 +132,14 @@ if __name__ == "__main__":
             msg += str(rule) + "\n"
         msg += "========== Streaming rules end =======\n"
         logger.info(msg)
-    
-    # stream.filter()
-    stream.sample()
-    
+
+    stream.filter(
+        tweet_fields=[
+            "author_id",
+            "entities",
+            "public_metrics",
+            "created_at",
+            "lang",
+        ]
+    )
+    # stream.sample()
